9c138708510d94489e7d24a9e698d1cc838fbd02
Rio Cantre <smittenaf301icu@gmail.com>
2021-10-01 22:04:20 +0200

    Added bcrypt

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const bcrypt = require('bcrypt-nodejs');
 
 app.use(bodyParser.json());
 
@@ -22,6 +23,13 @@ const database = {
             entries: 0,
             joined: new Date()
         }
+    ],
+    login: [
+        {
+            id: '987',
+            hash: '',
+            email: 'john@gmail.com'
+        }
     ]
 }
 
@@ -39,8 +47,11 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
+    bcrypt.hash(password, null, null, function (err, hash) {
+        console.log(hash);
+    });
     database.users.push({
-      id: '123',
+      id: '125',
       name: name,
       email: email,
       password: password,
@@ -78,6 +89,20 @@ app.post('/image', (req, res) => {
       res.status(400).json('not found');
     }
 })
+
+bcrypt.hash('bacon', null, null, function (err, hash) {
+  // Store hash in your password DB.
+});
+
+// // Load hash from your password DB.
+// bcrypt.compare('bacon', hash, function (err, res) {
+//   // res == true
+// });
+// bcrypt.compare('veggies', hash, function (err, res) {
+//   // res = false
+// });
+
+
 app.listen(3000, () => {
     console.log('app is running on port 3000');
 })
