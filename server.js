59f6f67663fb32589402fd2896804a45e9607110
Rio Cantre <smittenaf301icu@gmail.com>
2021-10-10 20:48:49 +0200

    Updated files

--- server.js
+++ server.js
@@ -16,25 +16,23 @@ const db = knex({
     port: 5432,
     user: 'riocantre',
     password: '',
-    database: 'smart-brain',
+    database: 'smart-brain'
   },
-});
-
-db.select('*').from('users').then(data => {
-    console.log(data);
 });
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(database.user) })
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+app.get('/', (req, res) => { res.send(database.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
-app.put('/image', (req, res) => { image.handleImage(req, res, db) });
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
-})
+
+const port = process.env.PORT || 8888;
+app.listen(port, () => {
+  console.log(`server is running on localhost: ${port}`);
+});
 
